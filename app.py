4bd947c7496526c43291bcda549aae5edd3880df
thilak9 <b.thilakshan9@gmail.com>
2022-08-16 16:05:18 +0100

    Tried SQLAlchemy

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from controllers import contacts
 from werkzeug import exceptions
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+
+
+class Contacts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    number = db.Column(db.String(120), unique=False, nullable=False)
+    # email = db.Column(db.String(120), unique=True, nullable=False)
+    workplace = db.Column(db.String(80), unique=False, nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.name
+
+
+
+from controllers import contacts
+
 
 @app.route('/')
 def welcome():
@@ -13,21 +32,21 @@ def welcome():
 @app.route('/api/contacts', methods = ['GET', 'POST'])
 def contact():
     fns = {
-        'GET': contacts.index,
-        'POST': contacts.create
+        'GET': contacts.index
+        # 'POST': contacts.create
     }
     resp, code = fns[request.method](request)
     return jsonify(resp), code
 
-@app.route('/api/contacts/<int:contact_id>', methods=['GET', 'DELETE','PUT'])
-def contact_handler(contact_id):
-    fns = {
-        'GET': contacts.show,
-        'DELETE': contacts.destroy,
-        'PUT': contacts.update
-    }
-    resp, code = fns[request.method](request, contact_id)
-    return jsonify(resp), code
+# @app.route('/api/contacts/<int:contact_id>', methods=['GET', 'DELETE','PUT'])
+# def contact_handler(contact_id):
+#     fns = {
+#         'GET': contacts.show,
+#         'DELETE': contacts.destroy,
+#         'PUT': contacts.update
+#     }
+#     resp, code = fns[request.method](request, contact_id)
+#     return jsonify(resp), code
 
 @app.errorhandler(exceptions.NotFound)
 def handle_404(err):
@@ -41,5 +60,7 @@ def handle_400(err):
 def handle_500(err):
     return {'message': f"It's not you, it's us {err}"}, 500
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
--- controllers/contacts.py
+++ controllers/contacts.py
@@ -1,40 +1,56 @@
 from werkzeug.exceptions import BadRequest
 
+from app import app,db
+db.create_all()
 
-contacts = [
-    {'id':1, 'name': 'Bob', 'number':'07632421834', 'email': 'bob@gmail.com', 'workplace': 'Electric and co.'},
-    {'id':2, 'name': 'Tim', 'number':'07834567912', 'email': 'tim@gmail.com', 'workplace': 'Plumber and co.'},
-    {'id':3, 'name': 'Bill', 'number':'07541945678', 'email': 'bill@gmail.com', 'workplace': 'Fish and Chips co'},
-    {'id':4, 'name': 'Jimmy', 'number':'07894567431', 'email': 'jimmy@gmail.com', 'workplace': 'Hospital and co'}
-]
+from app import Contacts
+
+Contacts.__table__.drop()
+
+db.session.add(Contacts(id=1, name='Bob', number='07632421834', workplace='Electric and co.'))
+db.session.commit()
+
+
+
+# contacts = [
+#     {{'id':1, 'name': 'Bob', 'number':'07632421834', 'email': 'bob@gmail.com', 'workplace': 'Electric and co.'}},
+#     {'id':2, 'name': 'Tim', 'number':'07834567912', 'email': 'tim@gmail.com', 'workplace': 'Plumber and co.'},
+#     {'id':3, 'name': 'Bill', 'number':'07541945678', 'email': 'bill@gmail.com', 'workplace': 'Fish and Chips co'},
+#     {'id':4, 'name': 'Jimmy', 'number':'07894567431', 'email': 'jimmy@gmail.com', 'workplace': 'Hospital and co'}
+# ]
+
+
+
+
 
 def index(req):
-    return [c for c in contacts], 200
+    # return [c for c in contacts], 200
+    return Contacts.query.all(), 200
 
-def create(req):
-    new_contact = req.get_json()
-    new_contact['id'] = sorted([c['id'] for c in contacts])[-1] +1
-    contacts.append(new_contact)
-    return new_contact, 201
+# def create(req):
+#     new_contact = req.get_json()
+#     new_contact['id'] = sorted([c['id'] for c in contacts])[-1] +1
+#     contacts.append(new_contact)
+#     return new_contact, 201
 
-def show(req, uid):
-    return find_by_uid(uid), 200
+# def show(req, uid):
+#     return find_by_uid(uid), 200
 
-def destroy(req, uid):
-    contact = find_by_uid(uid)
-    contacts.remove(contact)
-    return contact, 204
+# def destroy(req, uid):
+#     contact = find_by_uid(uid)
+#     contacts.remove(contact)
+#     return contact, 204
 
-def update(req, uid):
-    contact = find_by_uid(uid)
-    data = req.get_json()
-    print(data)
-    for key, val in data.items():
-        contact[key] = val
-    return contact, 200
+# def update(req, uid):
+#     contact = find_by_uid(uid)
+#     data = req.get_json()
+#     print(data)
+#     for key, val in data.items():
+#         contact[key] = val
+#     return contact, 200
 
-def find_by_uid(uid):
-    try: 
-        return next (c for c in contacts if c['id'] == uid)
-    except:
-        raise BadRequest(f"We dont have a contact with id {uid}!")
+# def find_by_uid(uid):
+#     try: 
+#         return next (c for c in contacts if c['id'] == uid)
+#     except:
+#         raise BadRequest(f"We dont have a contact with id {uid}!")
